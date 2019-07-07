d5e73c40157a38078407a98b665f05219fe2f08b
Adilson <adilson.pacheco93@gmail.com>
2019-07-07 17:24:21 +0100

    update new fileas

--- js/loadJSON.js
+++ js/loadJSON.js
@@ -1,117 +1,75 @@
-// create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions can use the same variable
-var client;
-// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
-var layer;
-// Parsed JSON
-var crimejson;
-// Load the map
-getCrimes();
-//Select dates function
-datePicker();
+$(document).ready(
 
-// create the code to get the Earthquakes data using an XMLHttpRequest 
-function getCrimes() {
-	client = new XMLHttpRequest();
-	client.open('GET','https://data.cityofchicago.org/resource/vwwp-7yr9.geojson?$limit=200'); 
-	client.onreadystatechange = crimeResponse; 
-// note don't use earthquakeResponse() with brackets as that doesn't work 
-client.send();
-}
-// create the code to wait for the response from the data server, and process the response once it is received
-function crimeResponse() {
-// this function listens out for the server to say that the data is ready - i.e. has state 4 
-	if (client.readyState == 4) {
-// once the data is ready, process the data 
-		var crimedata = client.responseText; 
-		loadCrimelayer(crimedata);
-		crimeLayer(crimedata);
-		filterData(crimedata);
-	}
-}
-// define a global variable to hold the layer so that we can use it later on
-// convert the received data - which is text - to JSON format and add it to the map 
-function loadCrimelayer(crimedata) {
-// convert the text to JSON
-	crimejson = JSON.parse(crimedata);
-// add the JSON layer onto the map - it will appear using the default icons 
-	layer = L.geoJson(crimejson,
-	{
-		//use point to layer to create the points
-		pointToLayer: function (feature,latlng)
-		{
-		// look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
-		// also include a pop-up that shows the place value of the earthquakes 
-		return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.primary_type +"</b>");
-	},
-	}).addTo(map); 
-	map.fitBounds(layer.getBounds());
-}
-
-// define a global variable to hold the layer so that we can use it later on
-var offenceDrop;
-var arrestDrop;
-var domesticDrop;
-
-function crimeLayer(crimedata) {
-// convert the text to JSON
-	data = crimejson.features
-	// console.log(data)
-	// https://www.youtube.com/watch?v=nmNvGHMtE2k - make Offence group array unique
-	let arrOffence = data.map(item => item.properties.primary_type);
-	arrOffence =  [...new Set(arrOffence)];
-	arrOffence = arrOffence.sort();
-	console.log(arrOffence)
-
-	var offenceDrop = document.getElementById("offence");
-	for(var i = 0; i < arrOffence.length; i++){
-		offenceDrop.options.add(new Option(arrOffence[i]));
-	}
+	function() {
+	var offSelect = $("#crimeType")
 	
-	// https://www.youtube.com/watch?v=nmNvGHMtE2k - make Arrest array unique
-	let arrArrest = data.map(item => item.properties.arrest);
-	arrArrest =  [...new Set(arrArrest)];
-	arrArrest = arrArrest.reverse();
-	// console.log(arrArrest.touppercase())
+	$.ajax({
+		url: 'https://data.cityofchicago.org/resource/vwwp-7yr9.geojson?$select=distinct primary_type&$order=primary_type ASC'
+	})
+	.done(function(data) {
+		// console.log(data);
+		$.each(data.features, function (i, feature) {
+			// console.log(i,feature);
+			offSelect.append(
+				$("<option></option>").val(feature.properties.primary_type).html(feature.properties.primary_type))
+		});
 
-	var arrestDrop = document.getElementById("arrest");
-	for(var i = 0; i < arrArrest.length; i++){
-		arrestDrop.options.add(new Option(arrArrest[i]));
-	}
+		console.log(data);
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
 
-		// https://www.youtube.com/watch?v=nmNvGHMtE2k - make Arrest array unique
-	let arrDomestic = data.map(item => item.properties.domestic);
-	arrDomestic = [...new Set(arrDomestic)];
-	arrDomestic = arrDomestic.reverse();
-	// console.log(arrDomestic)
 
-	var domesticDrop = document.getElementById("domestic");
-	for(var i = 0; i < arrDomestic.length; i++){
-		domesticDrop.options.add(new Option(arrDomestic[i]));
-	}
+	var arrSelect = $("#arrest")
+	$.ajax({
+		url: 'https://data.cityofchicago.org/resource/vwwp-7yr9.geojson?$select=distinct arrest&$order=arrest DESC'
+	})
+	.done(function(data) {
+		console.log(data);
+		$.each(data.features, function (i, feature) {
+			console.log(i,feature);
+			arrSelect.append(
+				$("<option></option>").val(feature.properties.arrest).html(feature.properties.arrest))
+			// body...
+		});
+		console.log(data);
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
 
-}
+	var domSelect = $("#domestic")
+	$.ajax({
+		url: 'https://data.cityofchicago.org/resource/vwwp-7yr9.geojson?$select=distinct domestic&$order=domestic DESC'
+	})
+	.done(function(data) {
+		console.log(data);
+		$.each(data.features, function (i, feature) {
+			console.log(i,feature);
+			domSelect.append(
+				$("<option></option>").val(feature.properties.domestic).html(feature.properties.domestic))
+			// body...
+		});
+		console.log(data);
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
 
-function getValue() {
-	// body...
-	// var dropVal = offenceDrop.options[offenceDrop.selectedIndex].value;
-	var offenceVal = document.getElementById("offence").value;
-	// console.log(offenceVal);
-	var arrestVal = document.getElementById("arrest").value;
-	// console.log(arrestVal);
-	var domesticVal = document.getElementById("domestic").value;
-	// console.log(domesticVal);
 
-	var startDateVal = document.getElementById("startDate").value;
-	// console.log(startDateVal);
-
-	var endDateVal = document.getElementById("endDate").value;
-	dateArr= [...new Array(startDateVal,endDateVal)]
-
-}
-
-var startDate, endDate; 
-
-function datePicker() {
     $('#startDate').datepicker({
     	showAnim: 'slideDown',
     	changeMonth: true,
@@ -131,10 +89,92 @@ function datePicker() {
     		$( "#startDate" ).datepicker( "option", "maxDate", selectedDate );
 	  	}
 	});
+
+	});
+
+function getOffence(){
+	var offence= $("#crimeType").val();
+	// console.log(Offence);
+	fitlerData(offence);
 };
 
-function filterData(crimedata) {
-	// body...
-	data= crimejson.features;
-	let filerData = data.map(item => item.properties);
+function getArrest(){
+	var arrest= $("#arrest").val();
+	// console.log(arrest);
+	fitlerData(arrest);
+};
+
+function getDomestic(){
+	var domestic= $("#domestic").val();
+	// console.log(domestic);
+	fitlerData(domestic);
+};
+
+function getStartDate(){
+	var startDate= $("#startDate").val();
+	// var selectedEnd= $("#endDate").val();
+	// console.log(selectedStart);
+	fitlerData(startDate);
+};
+
+function getEndDate(){
+	var endDate= $("#endDate").val();
+	// console.log(endDate);
+	fitlerData(endDate);
+};
+
+function fitlerData(offence,arrest,domestic,startDate,endDate) {
+
+	$.ajax({
+		url: 'https://data.cityofchicago.org/resource/vwwp-7yr9.geojson?',
+		method: "GET",
+		dataType: "json",
+        data: {
+          "$where": "primary_type = '" + offence + "'",
+          // + " AND date >'" + startDate + "'",
+          // + " AND date <'" + endDate + "'"
+          // + " AND primary_type = '" + offence + "'" 
+          // + " AND arrest = " + arrest 
+          // + " AND domestic = " + domestic,
+          "$limit" : 200
+      	},
+	})
+	.done(function(data) {
+
+		// $.each(data.features, function (i, feature) {
+		// 	console.log(i,feature);
+		// 	domSelect.append(
+		// 		$("<option></option>").val(feature.properties.domestic).html(feature.properties.domestic))
+		// 	// body...
+		// });
+		// add the JSON layer onto the map - it will appear using the default icons 
+		$("#crimeType").change(function() {
+			/* Act on the event */
+			layer = L.geoJson(data,{
+			//use point to layer to create the points
+				pointToLayer: function (feature,latlng){
+			return new L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.primary_type +"</b>");
+			},
+			}).addTo(map); 
+			map.fitBounds(layer.getBounds());
+
+		});
+		// layer = L.geoJson(data,
+		// {
+		// 	//use point to layer to create the points
+		// 	pointToLayer: function (feature,latlng)
+		// 	{
+		// 	// look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
+		// 	// also include a pop-up that shows the place value of the earthquakes 
+		// 	return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.primary_type +"</b>");
+		// },
+		// }).addTo(map); 
+		// map.fitBounds(layer.getBounds());
+		console.log(data);
+		console.log("success");
+	})
+
 }
+
+
+
